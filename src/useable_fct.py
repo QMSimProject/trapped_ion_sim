92615e5328e937450225914fcbd7b2ea7e2125d1
mskoenz <mskoenz@gmx.net>
2014-07-24 09:32:01 +0200

    13.Commit:	more files split mskoenz
    DONE:
    - added usable_fct.py for all fct that dont require the namespace/module prefix while parsing
    - now one can use Qobj in the states fields of the GUI

---
+++ src/useable_fct.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+# 
+# Author:  Mario S. Könz <mskoenz@gmx.net>
+# Date:    24.07.2014 08:39:15 CEST
+# File:    useable_fct.py
+
+from qutip import fock_dm
+from qutip import coherent_dm
+from qutip import thermal_dm
+from qutip import squeeze
+from qutip import destroy
+from qutip import create
+from qutip import sigmax
+from qutip import sigmay
+from qutip import sigmaz
+from qutip import qeye
+from qutip import num
+from numpy import pi
+from numpy import diag
+from numpy import sqrt
